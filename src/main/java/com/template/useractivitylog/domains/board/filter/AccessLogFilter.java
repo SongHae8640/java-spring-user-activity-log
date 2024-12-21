2adb49cf5e1eb8be83423b97cfc8f23c5c9b4115
SongHae8640 <thdgo456@naver.com>
2024-12-21 14:22:44 +0900

    etc : Lombok 적용

--- src/main/java/com/template/useractivitylog/domains/board/filter/AccessLogFilter.java
+++ src/main/java/com/template/useractivitylog/domains/board/filter/AccessLogFilter.java
@@ -6,13 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Slf4j
 public class AccessLogFilter extends OncePerRequestFilter {
-
-  private static final Logger log = LoggerFactory.getLogger(AccessLogFilter.class);
 
   @Override
   protected boolean shouldNotFilter(HttpServletRequest request) {
